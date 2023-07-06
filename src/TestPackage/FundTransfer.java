7f496b6fe53d5e38c8616aa0aa6a3465057ed9f2
Shaik Anees <71702294+testadmin155@users.noreply.github.com>
2023-07-07 00:09:13 +0200

    Update FundTransfer.java

--- src/TestPackage/FundTransfer.java
+++ src/TestPackage/FundTransfer.java
@@ -5,6 +5,7 @@ public class FundTransfer {
 	public static void main(String[] args) {
 		
 		System.out.println("This is a Fund Transfer");
+		System.out.println("This is a Fund Transfer to other Account");
 
 	}
 
