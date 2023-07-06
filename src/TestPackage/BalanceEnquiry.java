7e7ca6a4609a72bc0ddae7995f9f3c659e4cde69
Anees <skanees007@gmail.com>
2023-07-07 00:07:39 +0200

    Second COmmit

---
+++ src/TestPackage/BalanceEnquiry.java
@@ -0,0 +1,12 @@
+package TestPackage;
+
+public class BalanceEnquiry {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is test for Balance Enquiry");
+		System.out.println("My second test case");
+
+	}
+
+}
